d9da800c0ec425a2e45ddb5b9bf6c08c2f27bcc0
ssgrummons <ssgrummo@us.ibm.com>
2025-04-16 18:00:31 -0400

    Clean up placeholder files

--- backend/src/prompt_utils.py
+++
@@ -1,8 +0,0 @@
-from langchain.schema.messages import SystemMessage, HumanMessage
-
-def build_messages(system_prompt: str, user_prompt: str):
-    messages = []
-    if system_prompt:
-        messages.append(SystemMessage(content=system_prompt))
-    messages.append(HumanMessage(content=user_prompt))
-    return messages
